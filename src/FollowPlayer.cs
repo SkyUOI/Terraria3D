f80499ca7fe8184ac4979034825d683b89270e61
limuy2022 <limuy202011@163.com>
2025-07-03 17:38:59 +0800

    camera

--- src/FollowPlayer.cs
+++ src/FollowPlayer.cs
@@ -3,4 +3,14 @@ using System;
 
 public partial class FollowPlayer : Camera3D
 {
+    public override void _Ready()
+    {
+        base._Ready();
+    }
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+        Position = GetNode<Player>("../Player").Position;
+    }
 }
